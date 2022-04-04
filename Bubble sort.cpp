7ee3a5fff37aa655b5555081ce9a8762c4dd40fa
Cris-19 <81449781+Cris-19@users.noreply.github.com>
2022-04-04 16:03:37 -0300

    Add files via upload

---
+++ Bubble sort.cpp
@@ -0,0 +1,28 @@
+
+#include <iostream>
+using namespace std;
+int main(){    
+    int arr[] = {9, 5, 2,7,1}; // Array exemplo para a ordenação dos dados
+    int n = sizeof(arr)/sizeof(arr[0]);
+    // Loop para realizar as operacoes ate que todos os elementos do array seja ordenados
+    cout << "Array original :" <<" " ;
+    for (int i = 0; i < n; i++){
+          cout << arr[i] << " ";
+    }
+cout<<" "<<endl; 
+    for (int i=0;i<(n-1);i++){
+        // Loop para a comparacao de dois elementos
+        for (int j=0;j<(n-1);j++){   
+            if (arr[j+1]<arr[j]){
+                swap(arr[j],arr[j+1]); // Funcao swap para realizar a troca os elementos de lugares
+            } 
+        }
+    }
+    cout << "Array ordenado :" <<" " ;
+    //Loop para imprimir os elementos do array ordenado   
+    for (int i=0;i<(n);i++){         
+        cout<<arr[i]<<" "; 
+    }
+    return 0;
+}
+
