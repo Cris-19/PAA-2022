f8ce2b3986a384c909e7020ee8a3ec373b6dd9d4
Cris-19 <81449781+Cris-19@users.noreply.github.com>
2022-04-05 20:18:56 -0300

    Add files via upload

---
+++ Problema da mochila.cpp
@@ -0,0 +1,48 @@
+   #include <iostream>
+   #include <iostream>
+   using namespace std;
+
+   int main() {
+   int max(int a, int b);
+   int knapSack(int W, int wt[], int val[], int n);
+   int val[] = { 10, 20, 80 };
+   int W = 80;              
+    int n = sizeof(val) / sizeof(val[0]);
+    
+    for(int w = 0; w <= W; w++);   	// inicializando a primeira linha e primeira coluna com 0
+	for(int i = 1; i <= n; i++);
+
+	for(int i = 1; i <= n; i++);   //
+	
+	for(int w = 1; w <= W; w++);
+		
+
+    cout << "knapSack int W, int wt, int val, int n" ;
+    cout << "knapsack" ;
+    
+    	// capacidade máxima da mochila: W
+	 int Wt= {20};
+
+	// vetor com os valores (benefício) de cada elemento
+	int b[] = {3, 5, 8, 4, 10};
+
+	// vetor com os pesos de cada elemento
+	int wt[] = {2, 4, 5, 3, 9};
+
+	// obtém o máximo valor que pode ser colocado na mochila
+	int knapsack(int W, int wt[], int b[],int  n[] );
+	
+	cout << "Valor maximo: " ;
+
+	cout << "20";
+
+
+    
+    
+        return 0; 
+		}
+
+
+
+
+    
